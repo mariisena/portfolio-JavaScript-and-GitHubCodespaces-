1adc3354ce6f32b93e5d2c106164cec719fc4dc3
Mariana Sena <marianasena@sempreceub.com>
2024-09-24 22:31:55 +0000

    primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mariana Sena",
+  title: "Estudante de Ciência da Computação",
+  email: "marianasena@sempreceub.com",
+  gitHub: "https://github.com/mariisena",
+  instagram: "https://intagram.com/marirsena",
+  linkedIn: "https://www.linkedin.com/in/mariana-rodrigues-sena-952637b0/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/ai-generated.png";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,27 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "Imersão Dev_ Alura + Google Gemini🎉",
+    description: "Aprendendendo HTML, CSS e JavScript.",
+    url: "https://meu-primeiro-siteimersao-dev.vercel.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Trabalho 2",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Descrição 2 - Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+    url: "",
   },
   {
-    title: "My Resume Site",
+    title: "Trabalho 3",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Descrição futura 3 - Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+    url: "",
   },
   {
     title: "GitHub Codespaces and github.dev",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Descrição futura 4 - Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+    url: "",
   },
 ];
 
